a806005d11882fdeb5dc6d9219c4ffbbb833e1b8
lumieria <private>
2016-08-15 23:57:44 -0500

    Added showHwSerialNum.sh

---
+++ showHwSerialNum.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+# This script reads the serial number from the i2c-connected eeprom available
+# on BeagleBone (Black). It should work both on device-tree and pre-device-tree
+# kernels.
+#
+# The serial number is unique for each BeagleBone (Black) and also includes
+# the week/year of manufacture in the first 4 digits.
+#
+# I only tested this on Ubuntu, but it should probably work on other distros
+# as well.
+#
+
+notif () {
+   echo "\033[1;35m${1}\033[0m${2}"
+}
+
+fail () {
+   echo "\033[1;31m${1}\033[0m${2}"
+   exit 0
+}
+
+checks () {
+   if ! [ $(id -u) = 0 ]; then
+      fail "you need to be root to run this (or use sudo)."
+   fi
+   
+   has_hexdump=$(which hexdump 2>/dev/null)
+   if [ ! "${has_hexdump}" ]; then
+      fail "you need to install the BSD utils (apt-get install bsdmainutils)."
+   fi
+}
+
+bbbserial () {
+   EEPROM="/sys/bus/i2c/devices/1-0050/eeprom"
+   
+   if [ ! -f "${EEPROM}" ]; then
+      EEPROM="/sys/bus/i2c/devices/0-0050/eeprom"
+   fi
+  
+   if [ ! -f "${EEPROM}" ]; then
+      EEPROM="/sys/bus/i2c/devices/0-0050/at24-0/nvmem"
+   fi
+ 
+   if [ ! -f "${EEPROM}" ]; then
+      fail "i2c eeprom file not found in sysfs."
+   fi
+
+   if [ "$1" == "--old" ]; then
+       # OLD METHOD: special case for rs01, only if specified by 
+       SERIAL=$(hexdump -e '8/1 "%c"' "${EEPROM}" -s 80 -n 9 2>&1)
+       if [ "${SERIAL}" = "${SERIAL#*BB}" ]; then
+          fail "failed to extract serial number from i2c eeprom: " "${SERIAL}"
+       fi
+   else
+       # NEW METHOD: matchs method '0' in aHw.C
+       SERIAL=$(hexdump -e '8/1 "%c"' "${EEPROM}" -s 16 -n 12 2>&1)
+       if [ "${SERIAL}" = "${SERIAL#*BB}" ]; then
+          fail "failed to extract serial number from i2c eeprom: " "${SERIAL}"
+       fi
+   fi
+   echo "${SERIAL}"
+}
+#verbose=true
+os=$(uname -s)
+bbbserial
+exit 0
+
